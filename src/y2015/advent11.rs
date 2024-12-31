4e189e2b65271816e25ce88be4ee6167aea85d38
Maria Pu <dame.schwinge@gmail.com>
2024-12-31 01:53:42 +0100

    AoC15.d11.p2

--- src/y2015/advent11.rs
+++ src/y2015/advent11.rs
@@ -31,47 +31,68 @@ impl Solve for Advent {
         Ok(())
     }
 
-    fn compute_part1_answer(&self, _test_mode: bool) -> Result<String, String>{
+    fn compute_part1_answer(&self, _: bool) -> Result<String, String>{
         self.check_input(Some(1))?;
-        let forbidden = vec!['i','l','o'];
-        let alphabet: Vec<char> = ('a'..='z').collect();
-        let mut input = self.input.clone();
-        let password_len = input.len();
-        loop {
-            if input==vec!['z';password_len]{
-                return Err(String::from("No solution found"))
-            }
-            // Check for forbidden characters
-            if input.iter().any(|&ch| forbidden.contains(&ch)) {
-                gen_next_password(&mut input);
-                continue;
-            }
-
-            // Check for a straight of three consecutive letters
-            if !alphabet.windows(3).any(|seq| input.windows(3).any(|win| win == seq)) {
-                gen_next_password(&mut input);
-                continue;
-            }
-
-            // Check for two different, non-overlapping pairs
-            let pairs: HashSet<_> = input
-                .windows(2)
-                .filter(|&pair| pair[0] == pair[1])
-                .map(|pair| pair[0])
-                .collect();
-            if pairs.len() < 2 {
-                gen_next_password(&mut input);
-                continue;
-            }
-
-            return assert_display(input.iter().join(""), None, String::from("hepxxyzz"), "Password", false)
+        match gen_next_correct_password(self.input.clone()){
+            Some(input)=> assert_display(input.iter().join(""),
+                                         None,
+                                         String::from("hepxxyzz"),
+                                         "Password", false),
+            None => Err(String::from("No solution found"))
         }
     }
-    //
-    // fn compute_part2_answer(&self,  _: bool) -> Result<String, String>{
-    //     self.check_input(Some(2))?;
-    //     Err(String::from("Part 2 not implemented yet"))
-    // }
+
+    fn compute_part2_answer(&self,  _: bool) -> Result<String, String>{
+        self.check_input(Some(2))?;
+        match gen_next_correct_password(self.input.clone()){
+            Some(mut input)=> {
+                gen_next_password(&mut input);
+                match gen_next_correct_password(input){
+                    Some(input)=> assert_display(input.iter().join(""),
+                                                 None,
+                                                 String::from("heqaabcc"),
+                                                 "Password", false),
+                    None => Err(String::from("No solution found"))
+                }
+            },
+            None => Err(String::from("No solution found"))
+        }
+    }
+}
+
+fn gen_next_correct_password(mut input: Vec<char>)->Option<Vec<char>>{
+    let forbidden = vec!['i','l','o'];
+    let alphabet: Vec<char> = ('a'..='z').collect();
+    let password_len = input.len();
+    loop {
+        if input==vec!['z';password_len]{
+            return None
+        }
+        // Check for forbidden characters
+        if input.iter().any(|&ch| forbidden.contains(&ch)) {
+            gen_next_password(&mut input);
+            continue;
+        }
+
+        // Check for a straight of three consecutive letters
+        if !alphabet.windows(3).any(|seq| input.windows(3).any(|win| win == seq)) {
+            gen_next_password(&mut input);
+            continue;
+        }
+
+        // Check for two different, non-overlapping pairs
+        let pairs: HashSet<_> = input
+            .windows(2)
+            .filter(|&pair| pair[0] == pair[1])
+            .map(|pair| pair[0])
+            .collect();
+        if pairs.len() < 2 {
+            gen_next_password(&mut input);
+            continue;
+        }
+
+        return Some(input);
+    }
 }
 
 fn gen_next_password(input: &mut Vec<char>) {
