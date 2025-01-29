77434e9d5391f6c479d53c7464f36934d2417c56
Maria Putintseva <maria.putintseva@robeco.com>
2025-01-29 22:29:08 +0100

    AoC16.d11 fast solution

--- src/y2016/advent11.rs
+++ src/y2016/advent11.rs
@@ -15,12 +15,65 @@ pub(crate) struct Advent {
     components: HashSet<(usize, Component)>,
     n_floors: usize
 }
+
 impl Default for Advent {
     fn default() -> Self{
         Self{
             label: Label::new(11, 2016),
             components: HashSet::new(),
             n_floors: 0
+        }
+    }
+}
+
+impl Advent {
+    fn solve(&self,
+             components: &HashSet<(usize, Component)>,
+             result_prd: usize,
+             part: u8
+    ) -> Result<String, String> {
+        self.check_input(Some(part))?;
+        let mut curr_state: Vec<Vec<Component>> = vec![Vec::new(); self.n_floors];
+        for (floor, component) in components{
+            curr_state.get_mut(*floor).unwrap().push(component.clone());
+        }
+        for set in curr_state.iter_mut(){
+            set.sort_unstable();
+        }
+        let curr_floor = 0usize;
+        let mut curr_step = 0usize;
+        let mut states: HashMap<(usize, Vec<[usize;3]>), usize> = HashMap::new();
+        states.insert((curr_floor, condense_state(&curr_state)), curr_step);
+
+        let mut queue = hashset!((curr_floor, curr_state));
+        let mut result: Option<usize> = None;
+        loop {
+            let mut queue_next: HashSet<(usize, Vec<Vec<Component>>)> = HashSet::new();
+            let mut found_end_state = false;
+            for (curr_floor, curr_state) in queue.iter(){
+                let next = next_states(curr_state, *curr_floor, self.n_floors, curr_step, &mut states);
+                for (next_floor, s) in next{
+                    if end_condition(&s, self.n_floors){
+                        found_end_state = true;
+                        result = Some(curr_step+1);
+                        break;
+                    }else{
+                        queue_next.insert((next_floor, s));
+                    }
+                }
+                if found_end_state{
+                    break;
+                }
+            }
+            queue = queue_next;
+            if queue.len() ==0{
+                break;
+            }
+            curr_step+=1;
+        }
+        match result {
+            None => Err(String::from("Not solution found")),
+            Some(n_steps) => assert_display(n_steps, None, result_prd, "Minimal number of steps", false)
         }
     }
 }
@@ -64,65 +117,81 @@ impl Solve for Advent {
         println!("Components {:?}", self.components);
         Ok(())
     }
+
     fn compute_part1_answer(&self, _test_mode: bool) -> Result<String, String>{
-        self.check_input(Some(1))?;
+        self.solve(&self.components, 37,1)
+    }
+    fn compute_part2_answer(&self, _test_mode: bool) -> Result<String, String>{
         let mut components = self.components.clone();
-        //part 2 answer is 61, current method super slow, probably need recursion
         components.insert((0, Component::Generator('e')));
         components.insert((0, Component::Generator('d')));
         components.insert((0, Component::Microchip('e')));
         components.insert((0, Component::Microchip('d')));
+        self.solve(&components, 61,2)
+    }
+}
 
-        let mut curr_state: Vec<Vec<Component>> = vec![Vec::new(); self.n_floors];
-        for (floor, component) in components.iter(){
-            curr_state.get_mut(*floor).unwrap().push(component.clone());
+fn check_state(curr_state: &Vec<Vec<Component>>)->bool{
+    for floor_components in curr_state{
+        let mut generators: Vec<&char> = Vec::new();
+        let mut microchips: Vec<&char> = Vec::new();
+        for el in floor_components{
+            match el {
+                Component::Generator(ch) => generators.push(ch),
+                Component::Microchip(ch) => microchips.push(ch),
+            }
         }
-        for set in curr_state.iter_mut(){
-            set.sort_unstable();
+        if generators.is_empty(){
+            continue;
         }
-        let curr_floor = 0usize;
-        let mut curr_step = 0usize;
-        let mut states: HashMap<(usize, Vec<Vec<Component>>), usize> = HashMap::new();
-        states.insert((curr_floor, curr_state.clone()), curr_step);
-
-        let mut queue = hashset!((curr_floor, curr_state));
-        let mut result: Option<usize> = None;
-        loop {
-            let mut queue_next: HashSet<(usize, Vec<Vec<Component>>)> = HashSet::new();
-            let mut found_end_state = false;
-            for (curr_floor, curr_state) in queue.iter(){
-                let next = next_states(curr_state, *curr_floor, self.n_floors, curr_step, &mut states);
-                for (next_floor, s) in next{
-                    if end_condition(&s, self.n_floors){
-                        found_end_state = true;
-                        result = Some(curr_step+1);
-                        break;
-                    }else{
-                        queue_next.insert((next_floor, s));
-                    }
-                }
-                if found_end_state{
-                    println!("ES{}", curr_step+1);
+        for m in microchips.iter(){
+            let mut orphan = true;
+            for g in generators.iter(){
+                if m==g{
+                    orphan = false;
                     break;
                 }
             }
-            queue = queue_next;
-            if queue.len() ==0{
-                break;
+            if orphan{
+                return false;
             }
-            println!("QL{}", queue.len());
-            curr_step+=1;
-        }
-        match result {
-            None => Err(String::from("Not solution found")),
-            Some(n_steps) => assert_display(n_steps, None,37, "Minimal number of steps", false)
         }
     }
-    fn compute_part2_answer(&self, _test_mode: bool) -> Result<String, String>{
-        self.check_input(Some(2))?;
-        Err(String::from("Not implemented"))
-    }
+    true
 }
+
+fn condense_state(curr_state: &Vec<Vec<Component>>)->Vec<[usize;3]>{
+    let mut result: Vec<[usize;3]> = Vec::new();
+    for floor_components in curr_state{
+        let mut generators: Vec<&char> = Vec::new();
+        let mut microchips: Vec<&char> = Vec::new();
+        for el in floor_components{
+            match el {
+                Component::Generator(ch) => generators.push(ch),
+                Component::Microchip(ch) => microchips.push(ch),
+            }
+        }
+        let mut repr = [0usize;3];
+        for m in microchips.iter(){
+            let mut orphan = true;
+            for g in generators.iter(){
+                if m==g{
+                    orphan = false;
+                    break;
+                }
+            }
+            if orphan{
+                repr[2]+=1;
+            }else{
+                repr[0]+=1;
+            }
+        }
+        repr[1]=generators.len()-repr[0];
+        result.push(repr);
+    }
+    result
+}
+
 
 fn end_condition(curr_state: &Vec<Vec<Component>>, n_floors: usize) ->bool{
     for ix in 0..n_floors-1{
@@ -133,7 +202,7 @@ fn end_condition(curr_state: &Vec<Vec<Component>>, n_floors: usize) ->bool{
     true
 }
 
-fn next_states(curr_state: &Vec<Vec<Component>>, curr_floor: usize, n_floors: usize, curr_step: usize, states: &mut HashMap<(usize, Vec<Vec<Component>>), usize>)->HashSet<(usize, Vec<Vec<Component>>)>{
+fn next_states(curr_state: &Vec<Vec<Component>>, curr_floor: usize, n_floors: usize, curr_step: usize, states: &mut HashMap<(usize, Vec<[usize;3]>), usize>) ->HashSet<(usize, Vec<Vec<Component>>)>{
     let components = curr_state.get(curr_floor).unwrap();
     let mut next_floors: Vec<usize> = Vec::new();
     for x in [-1isize, 1isize]{
@@ -146,8 +215,9 @@ fn next_states(curr_state: &Vec<Vec<Component>>, curr_floor: usize, n_floors: us
 
     let mut process_state = |next_state: Vec<Vec<Component>>, next_floor: usize, result: &mut HashSet<(usize, Vec<Vec<Component>>)>| {
         if check_state(&next_state) {
-            if states.get(&(next_floor, next_state.clone())).map_or(true, |&n| curr_step + 1 < n) {
-                states.insert((next_floor, next_state.clone()), curr_step + 1);
+            let key = (next_floor, condense_state(&next_state));
+            if states.get(&key).map_or(true, |&n| curr_step + 1 < n) {
+                states.insert(key, curr_step + 1);
                 result.insert((next_floor, next_state));
             }
         }
@@ -183,32 +253,4 @@ fn next_states(curr_state: &Vec<Vec<Component>>, curr_floor: usize, n_floors: us
     result
 }
 
-fn check_state(curr_state: &Vec<Vec<Component>>)->bool{
-    for floor_components in curr_state{
-        let mut generators: Vec<&char> = Vec::new();
-        let mut microchips: Vec<&char> = Vec::new();
-        for el in floor_components{
-            match el {
-                Component::Generator(ch) => generators.push(ch),
-                Component::Microchip(ch) => microchips.push(ch),
-            }
-        }
-        if generators.is_empty(){
-            continue;
-        }
-        for m in microchips.iter(){
-            let mut orphan = true;
-            for g in generators.iter(){
-                if m==g{
-                    orphan = false;
-                    break;
-                }
-            }
-            if orphan{
-                //println!("{:?}", (m, floor_components));
-                return false;
-            }
-        }
-    }
-    true
-}
+
