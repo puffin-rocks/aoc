2f32ed2e0afd13b48abed48ad2ce0104c93f7b46
Maria Putintseva <maria.putintseva@robeco.com>
2025-02-12 21:02:13 +0100

    AoC16.d22.p2 speeding up

--- src/y2016/advent22.rs
+++ src/y2016/advent22.rs
@@ -21,11 +21,6 @@ impl Solve for Advent {
     fn get_label_mut(&mut self) -> &mut Label {&mut self.label}
 
     fn add_record_from_line(&mut self, line: String) -> Result<(), std::num::ParseIntError>{
-        //Filesystem              Size  Used  Avail  Use%
-        // if line.starts_with("root") | line.starts_with("Filesystem"){
-        //     return Ok(())
-        // }
-
         let re = Regex::new(r"/dev/grid/node-x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+(\d+)%").unwrap();
         if let Some(captures) = re.captures(line.as_str()){
             let x = captures.get(1).unwrap().as_str().parse::<usize>()?;
@@ -68,7 +63,14 @@ impl Solve for Advent {
             }
         }
         //checking that separation holds
-        let ind_empty = find_empty(&used);
+        if used.iter().filter(|v| **v==0).count()>1{
+            return Err(String::from("Not solution found"));
+        }
+        let ind_empty = used.iter().position(|v| *v==0);
+        if ind_empty.is_none(){
+            return Err(String::from("Not solution found"));
+        }
+        let ind_empty = ind_empty.unwrap();
         let capacity_empty= avlb[ind_empty];
         let mut capacity_small:Vec<usize> = Vec::new();
         let mut used_small:Vec<usize> = Vec::new();
@@ -90,25 +92,14 @@ impl Solve for Advent {
         assert_eq!(used_large.iter().min().unwrap()>capacity_small.iter().max().unwrap(), true);
         assert_eq!(avlb_large.iter().max().unwrap()<used_small.iter().min().unwrap(), true);
 
-        let mut symbols: Vec<char> = Vec::new();
+        let mut blocks: HashSet<usize> = HashSet::new();
         for i in 0..n{
-            if i == max_x-1{
-                symbols.push('G');
-            }
-            else {
-                if used[i] > 0 {
-                    if used[i] <= capacity_empty {
-                        symbols.push('.');
-                    } else {
-                        symbols.push('#');
-                    }
-                } else {
-                    symbols.push('_');
-                }
+            if i != max_x-1 && used[i] > 0 && used[i] > capacity_empty{
+                blocks.insert(i);
             }
         }
 
-        let result = iterate((symbols, ind_empty), max_x, max_y);
+        let result = iterate(ind_empty, max_x-1, blocks, max_x, max_y);
         match result{
             None => Err(String::from("Not solution found")),
             Some(v) => assert_display(v, None, 215, "Min number of steps", false)
@@ -117,51 +108,87 @@ impl Solve for Advent {
     }
 }
 
-fn distance(idx1: usize, idx2: usize, max_x: usize)->usize {
-    let i1 = (idx1%max_x) as isize;
-    let j1 = (idx1/max_x) as isize;
-    let i2 = (idx2%max_x) as isize;
-    let j2 = (idx2/max_x) as isize;
-    ((i1 - i2).abs()  + (j1-j2).abs()) as usize
-}
-
-fn find_empty(used: &Vec<usize>)-> usize{
-    if used.iter().filter(|v| **v==0).count()>1{
-        unreachable!()
+fn _iterate(loc_e: usize, loc_g: usize, blocks: HashSet<usize>, max_x: usize, max_y:usize) -> Option<usize>{
+    fn distance(idx1: usize, idx2: usize, max_x: usize)->usize {
+        let i1 = (idx1%max_x) as isize;
+        let j1 = (idx1/max_x) as isize;
+        let i2 = (idx2%max_x) as isize;
+        let j2 = (idx2/max_x) as isize;
+        ((i1 - i2).abs()  + (j1-j2).abs()) as usize
     }
-    used.iter().position(|v| *v==0).unwrap()
+
+    //this solution is slightly slower
+    let mut states: HashMap<(usize, usize), usize> = HashMap::new();
+    states.insert( (loc_e, loc_g), 0);
+    let mut queue: BTreeSet<(usize, usize, usize, usize, usize)> = BTreeSet::new(); //|E-G|, |G-0|, step, symbols, loc E, loc G
+    queue.insert((distance(loc_g, loc_e, max_x), max_x-1, 0, loc_e, loc_g));
+    let mut solution: Option<usize> = None;
+    while let Some((_, _, step, loc_e, loc_g)) = queue.pop_first(){
+        if solution.unwrap_or(usize::MAX-1)+1<=step{
+            continue;
+        }
+        let i = (loc_e%max_x) as isize;
+        let j = (loc_e/max_x) as isize;
+        for (i_n, j_n) in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)] {
+            if i_n > -1 && i_n < max_x as isize && j_n > -1 && j_n < max_y as isize {
+                let idx_n = (i_n + j_n * (max_x as isize)) as usize;
+                if loc_e == 0 && idx_n == loc_g{
+                    match solution {
+                        None => {
+                            solution = Some(step+1);
+                        },
+                        Some(v)=>{
+                            if v>step+1{
+                                solution = Some(step+1);
+                            }
+                        }
+                    }
+                }
+                if !blocks.contains(&idx_n){
+                    let loc_g_next = if idx_n == loc_g{
+                        loc_e
+                    }else{
+                        loc_g
+                    };
+                    let state_next = (idx_n, loc_g_next);
+                    if states.get(&state_next).unwrap_or(&usize::MAX)>&(step+1){
+                        states.insert(state_next, step+1);
+                        queue.insert(( distance(idx_n, loc_g_next, max_x), distance(loc_g_next, 0, max_x), step+1, idx_n, loc_g_next));
+                    }
+                }
+            }
+        }
+    }
+    solution
 }
 
-fn iterate(state: (Vec<char>, usize), max_x: usize, max_y:usize) -> Option<usize>{
-    let mut states: HashSet<Vec<char>> = HashSet::new();
-    states.insert( state.0.clone());
-    let mut queue: Vec<(Vec<char>, usize)> = Vec::new();
-    queue.push(state);
+fn iterate(loc_e: usize, loc_g: usize, blocks: HashSet<usize>, max_x: usize, max_y:usize) -> Option<usize>{
+    let mut states: HashSet<(usize, usize)> = HashSet::new();
+    states.insert( (loc_e, loc_g));
+    let mut queue: Vec<(usize, usize)> = Vec::new();
+    queue.push((loc_e, loc_g));
     let mut step = 0;
     loop{
-        //println!("{:?}", (step, queue.len()));
-        let mut queue_next: Vec<(Vec<char>, usize)> = Vec::new();
-        for (symbols, loc_e) in queue.iter(){
+        let mut queue_next: Vec<(usize, usize)> = Vec::new();
+        for (loc_e, loc_g) in queue.iter(){
             let i = (*loc_e%max_x) as isize;
             let j = (*loc_e/max_x) as isize;
             for (i_n, j_n) in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)] {
                 if i_n > -1 && i_n < max_x as isize && j_n > -1 && j_n < max_y as isize {
                     let idx_n = (i_n + j_n * (max_x as isize)) as usize;
-                    if *loc_e == 0 && symbols[idx_n] == 'G'{
+                    if *loc_e == 0 && idx_n == *loc_g{
                         return Some(step+1);
                     }
-                    if symbols[idx_n] == '.' || symbols[idx_n] == 'G'{
-                        let mut symbols_next = symbols.clone();
-                        if idx_n > *loc_e {
-                            let (left, right) = symbols_next.split_at_mut(idx_n);
-                            std::mem::swap(&mut left[*loc_e], &mut right[0]);
+                    if !blocks.contains(&idx_n){
+                        let loc_g_next = if idx_n == *loc_g{
+                            loc_e
                         }else{
-                            let (left, right) = symbols_next.split_at_mut(*loc_e);
-                            std::mem::swap(&mut left[idx_n], &mut right[0]);
-                        }
-                        if !states.contains(&symbols_next){
-                            states.insert(symbols_next.clone());
-                            queue_next.push((symbols_next, idx_n));
+                            loc_g
+                        };
+                        let state_next = (idx_n, *loc_g_next);
+                        if !states.contains(&state_next){
+                            states.insert(state_next);
+                            queue_next.push(state_next);
                         }
                     }
                 }
